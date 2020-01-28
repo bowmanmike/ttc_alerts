697307d78fd0d027e761b09ab97a7b5459af2e92
Mike Bowman <mbowman2@gmail.com>
2020-01-27 22:15:36 -0500

    Increase postgrex timeout in tests

--- config/test.exs
+++ config/test.exs
@@ -2,11 +2,12 @@ use Mix.Config
 
 # Configure your database
 config :ttc_alerts, TtcAlerts.Repo,
-  username: "postgres",
-  password: "postgres",
   database: "ttc_alerts_test",
   hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  ownership_timeout: 999_999,
+  password: "postgres",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  username: "postgres"
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
