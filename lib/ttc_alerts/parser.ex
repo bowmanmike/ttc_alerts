e0b98a8b9a73fada660a90917da4823f1b844bd3
Mike Bowman <mbowman2@gmail.com>
2020-03-21 17:17:42 -0400

    Add parser and test

---
+++ lib/ttc_alerts/parser.ex
@@ -0,0 +1,10 @@
+defmodule TtcAlerts.Parser do
+  @moduledoc """
+  Holds functionality to convert HTML documents parsed by
+  Floki into Elixir-native maps
+  """
+
+  def run(elements) do
+    elements
+  end
+end
---
+++ test/ttc_alerts/parser_test.exs
@@ -0,0 +1,11 @@
+defmodule TtcAlerts.ParserTest do
+  use TtcAlerts.DataCase, async: true
+
+  alias TtcAlerts.Parser
+
+  describe "run/1" do
+    test "it works" do
+      assert false
+    end
+  end
+end
