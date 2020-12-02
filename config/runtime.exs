220d72141da01b5da2be4f4e0e36a1add606df71
Mike Bowman <mbowman2@gmail.com>
2020-12-01 19:10:07 -0500

    Don't crash on missing variable

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,12 +6,7 @@ config :ttc_alerts, TtcAlerts.Repo,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+secret_key_base = System.get_env("SECRET_KEY_BASE")
 
 config :ttc_alerts, TtcAlertsWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
