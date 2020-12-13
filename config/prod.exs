4dac6692fc57644aef8c3aa987c5643b20a2d7b7
Mike Bowman <mbowman2@gmail.com>
2020-12-13 14:41:34 -0500

    Try to fix ssl

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :ttc_alerts, TtcAlertsWeb.Endpoint,
-  url: [host: "ttc-alerts.bowmanmike.com", port: 80],
+  url: [host: "ttc-alerts.bowmanmike.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
