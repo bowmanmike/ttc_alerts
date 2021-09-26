e6085f85cf3822583d51af636eef5c1a985b02ce
Mike Bowman <mbowman2@gmail.com>
2021-09-26 12:09:48 -0400

    Try to fix prod css

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,24 +1,6 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
+  plugins: [ 
+    require('autoprefixer'),
+    require('tailwindcss')('./tailwind.config.js')
+  ]
 }
-// module.exports = (ctx) => ({
-//   map: {
-//     absolute: false,
-//     sourcesContent: ctx.env === 'development'
-//   },
-//   plugins: {
-//     'postcss-import': { root: ctx.file.dirname },
-//     // commented out and running tailwindcss as its own watcher for now until a
-//     // newer release of postcss-cli (v8.0.1 or something) that contains
-//     // https://github.com/postcss/postcss-cli/pull/383
-//     // see: https://tailwindcss.com/docs/just-in-time-mode#it-just-doesn-t-seem-to-work-properly
-//     // tailwindcss: { config: './tailwind.config.js' }
-//     autoprefixer: {},
-//     tailwindcss: {}
-//     // cssnano: ctx.env === 'production' ? {preset: 'default'} : false
-//     // require("tailwindcss")("./tailwind.config.js"),
-//   }
-// })
--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   mode: 'jit',
-  purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
+  purge:  [
+    "./js/**/*.js",
+    "../lib/*_web/**/*.{heex,eex}",
+    "../lib/ttc_alerts_web/live/*.ex"
+  ],
   theme: {
     extend: {},
   },
